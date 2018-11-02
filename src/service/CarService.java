9833037e08000c184f25e9fe1582e76a3a0a1cb8
Arnold Herrera <arnoldjulianherreraquiones@gmail.com>
2018-11-02 01:00:03 -0500

    V1.0
    Se añade la interfaz POST mediante la cuál se puede añadir nuevos vehiculos a la base de datos en postgres.

--- src/service/CarService.java
+++ src/service/CarService.java
@@ -1,16 +1,12 @@
 package service;
 
-
-
-import javax.ws.rs.core.Response;
-
 import model.DTO.Car;
 
 public interface CarService {
 
-	public Response addCar(Car c);
+	public String addCar(Car c);
 
-	public String sellCar(String idEAN,int ammount);
+	public String sellCar(String idEAN, int ammount);
 
 	public Car getAllCar(String idEAN);
 
--- src/service/CarServiceImpl.java
+++ src/service/CarServiceImpl.java
@@ -1,9 +1,5 @@
 package service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,10 +7,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import controller.controllerCar;
-import model.GenericResponse;
+import model.DAO.CarroDAO;
 import model.DTO.Car;
 
 @Path("/car")
@@ -22,29 +17,21 @@ import model.DTO.Car;
 @Produces(MediaType.APPLICATION_XML)
 public class CarServiceImpl implements CarService {
 
-	private static Map<String, Car> cars = new HashMap<String, Car>();
 	private controllerCar carc = new controllerCar();
+
 	@Override
 	@POST
-	@Path("/newcar/")
-	public Response addCar(Car c) {
-		GenericResponse response = new GenericResponse();
-		if (cars.get(c.getEancode()) != null) {
-			response.setStatus(false);
-			response.setMessage("Car Already Exists!");
-			response.setErrorCode("CC-01");
-			return Response.status(422).entity(response).build();
-		}
-		cars.put(c.getEancode(), c);
-		response.setStatus(true);
-		response.setMessage("Car created succesfully");
-		return Response.ok(response).build();
+	@Path("/newcar")
+	public String addCar(Car c) {
+		CarroDAO carro = new CarroDAO();
+		carro.createcar(c);
+		return "hola";
 	}
 
 	@Override
 	@GET
 	@Path("/sell/{id}/{amount}")
-	public String sellCar(@PathParam("id") String idEAN,@PathParam("amount")int amount) {
+	public String sellCar(@PathParam("id") String idEAN, @PathParam("amount") int amount) {
 		carc.updateCar(idEAN, amount);
 		return "hecho actualizado";
 	}
@@ -52,13 +39,9 @@ public class CarServiceImpl implements CarService {
 	@Override
 	@GET
 	@Path("/query/{idEAN}")
-	public Car getAllCar(@PathParam("idEAN")String idEAN) {
+	public Car getAllCar(@PathParam("idEAN") String idEAN) {
 		Car c = carc.selectCar(idEAN);
 		return c;
 	}
-
-	
-
-
 
 }
